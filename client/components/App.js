225cca676ad703ccb61bc6553986dd953d9bd683
garrett hutson <hutsongarrett@gmail.com>
2023-02-22 17:08:08 -0500

    progress

--- client/components/App.js
+++ client/components/App.js
@@ -1,46 +1,54 @@
-
 import React, { useState, useEffect } from 'react';
-// import finnhub from 'finnhib'
-const finnhub = require('finnhub');
+import PortfolioTable from './PortfolioTable';
+import UserInfo from './UserInfo';
+import Login from './Login';
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import StockCarousel from './StockCarousel';
+import UserInfo from './UserInfo';
 
-const api_key = finnhub.ApiClient.instance.authentications['cfqkocpr01qigsfjuck0cfqkocpr01qigsfjuckg'];
-api_key.apiKey = "cfqkocpr01qigsfjuck0cfqkocpr01qigsfjuckg"
-const finnhubClient = new finnhub.DefaultApi()
+export default function App() {
 
 
-export default function App () {
+  //******************* state state state ******************************/
+  const stocks = ['AAPL', 'MSFT', 'AMZN', 'TSLA', 'GOOGL', 'META', 'YELP', 'ADBE', 'BABA', 'SONY']
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState('');
+  const [data, setData] = useState(null);
 
-    const [data, setData] = useState(null);
 
-    useEffect(() => {
-      finnhubClient.quote("AAPL", (error, data, response) => {
-        if (error) {
-          console.error(error);
-          return;
-        }
-        setData(data);
-      });
-const getData = async ()=>{
-    
-    const res =  await  fetch('finnhub.io/api/v1quote?symbol=AAPL',{
-        method: 'GET',
-        headers: {
-            'X-Finnhub-Token' : 'cfqkocpr01qigsfjuck0cfqkocpr01qigsfjuckg',
-            // 'Content-Type': 'Application/JSON'
-        }
-    })
-    res = res.json();
-    console.log(res)
-    setData(res)
+  // ********************* fetch requests ******************************//
+  useEffect(() => {
+    async function getData(stocks) {
+      const results = [];
+      for (let ticker of stocks) {
+        let res = await fetch(`https://finnhub.io/api/v1/quote?symbol=${ticker}&token=cfr5k41r01qhg1uraumgcfr5k41r01qhg1uraun0`, {
+          method: 'GET'
+        })
+        res = await res.json();
+        results.push(res);
+      }
+      setData(results);
+    }
+    getData(stocks);
+  }, []);
 
-}
-getData()
-
-    }, []);
-  
-    return (
-      <div>
-        {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
-      </div>
-    );
+  const getPrices = (stocks, data) => {
+    const pricesObj = {};
+    for (let i = 0; i < stocks.length; i++) {
+      const key = stocks[i];
+      const price = data[i].c;
+      pricesObj[key] = price
+    }
+    return pricesObj;
   }
+
+  return (
+    <div>
+      {/* <Login setUser={setUser} setLoggedIn={setLoggedIn}/> */}
+      <AccountCircleRoundedIcon />
+      <StockCarousel stocks={stocks} pricesObj={getPrices(data)}/>
+      <UserInfo user={user}/>
+      <PortfolioTable user={user} pricesObj={getPrices(data)}/>
+    </div>
+  );
+}
